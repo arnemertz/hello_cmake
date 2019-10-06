9cbc2efbcd9efe1e92402dc409eda6be49a19255
qvandenberg <quincy.vandenberg@physics.ox.ac.uk>
2019-10-06 16:43:20 +0100

    multiple test cases' q

--- test/testcases/test_FP.cpp
+++ test/testcases/test_FP.cpp
@@ -17,3 +17,17 @@ TEST_CASE( "hello() says hello" ) {
 
 	REQUIRE( out.str() == "Hello, tester!\nHello, you!\n");
 }
+
+TEST_CASE( "hello() says otherwise" ) {
+    std::ostringstream out;
+	std::streambuf* coutbuf = std::cout.rdbuf();
+
+    std::cout << "Running testcase otherwise()\n" << std::endl;
+
+    std::cout.rdbuf(out.rdbuf()); //redirect cout to out
+    hello("new test");
+	hello("us");
+    std::cout.rdbuf(coutbuf);
+
+	REQUIRE( out.str() == "Hello, new test!\nHello, us!\n");
+}
