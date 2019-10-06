a7300cfb4aeb5c8c1fe3db324a8a9ac9c4c11026
qvandenberg <quincy.vandenberg@physics.ox.ac.uk>
2019-10-06 16:26:10 +0100

    multiple test files

---
+++ test/test_FP.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+#include <sstream>
+#include <iostream>
+#include "hello.h"
+
+TEST_CASE( "hello() says hello" ) {
+    std::ostringstream out;
+	std::streambuf* coutbuf = std::cout.rdbuf();
+
+    std::cout << "Running testcase hello()\n" << std::endl;
+
+    std::cout.rdbuf(out.rdbuf()); //redirect cout to out
+    hello("tester");
+	hello("you");
+    std::cout.rdbuf(coutbuf);
+
+	REQUIRE( out.str() == "Hello, tester!\nHello, you!\n");
+}
