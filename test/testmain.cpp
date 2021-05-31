ad6de42a21ab79861b7eeeb4a56c32c062d84d6e
Mertz, Arne <Arne.Mertz@zuehlke.com>
2021-05-31 14:07:55 +0200

    add redirect console class

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -1,18 +1,38 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 #include <sstream>
 #include <iostream>
 #include "hello.h"
 
-TEST_CASE( "hello() says hello" ) {
-    std::ostringstream out;
-	std::streambuf* coutbuf = std::cout.rdbuf();
-	
-    std::cout.rdbuf(out.rdbuf()); //redirect cout to out
-    hello("tester");
-	hello("you");
-    std::cout.rdbuf(coutbuf);
+class RedirectConsole
+{
 
-	REQUIRE( out.str() == "Hello, tester!\nHello, you!\n");
+public:
+    explicit RedirectConsole(std::ostream &ostream)
+        : coutbuf{std::cout.rdbuf()}
+    {
+        std::cout.rdbuf(ostream.rdbuf()); //redirect cout to ostream
+    }
+
+    ~RedirectConsole()
+    {
+        std::cout.rdbuf(coutbuf);
+    }
+
+private:
+    std::streambuf *coutbuf;
+};
+
+TEST_CASE("hello() says hello")
+{
+    std::ostringstream out;
+    {
+        RedirectConsole console(out);
+
+        hello("tester");
+        hello("you");
+    }
+
+    REQUIRE(out.str() == "Hello, tester!\nHello, you!\n");
 }
